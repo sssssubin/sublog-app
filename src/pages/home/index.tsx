c9c22c4f76700d18ad24d77019741943eddfd0f1
forest-k <bstnqls@naver.com>
2024-05-19 00:01:24 +0900

    feat: 메인(홈) 컴포넌트 작업

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -10,11 +10,37 @@ export default function Home() {
           <Link to="/profile">프로필</Link>
         </div>
       </header>
-      <div className="post-list">Post List</div>
+      <div>
+        <div className="post__navigation">
+          <div className="post__navigation--active">전체</div>
+          <div>나의 글</div>
+        </div>
+      </div>
+      <div className="post__list">
+        {[...Array(10)].map((e, index) => (        
+          <div key={index} className="post__box">
+            <Link to={`/posts/${index}`}>
+              <div className="post__profile-box">
+                <div className="post__profile" />
+                <div className="post__author-name">김수빈</div>
+                <div className="post__date">yyyy.mm.dd</div>
+              </div>
+              <div className="post__title">게시글 {index}</div>  
+              <div className="post__text">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima dolorem accusamus, molestiae ea alias ab, repellendus natus tempora, quibusdam exercitationem blanditiis a quidem? Ipsa soluta consequuntur incidunt natus voluptate voluptates?
+              </div>
+            </Link>     
+            <div className="post__utils-box">
+              <div className="post__delete">삭제</div>
+              <div className="post__edit">수정</div>
+            </div>       
+          </div> 
+        ))}
+      </div>
       <footer>
-        <div>Menu 1</div>
-        <div>Menu 2</div>
-        <div>Menu 3</div>
+        <Link to="/posts/new">글쓰기</Link>
+        <Link to="/posts">게시글</Link>
+        <Link to="/profile">프로필</Link>
       </footer>
     </div>
   );
