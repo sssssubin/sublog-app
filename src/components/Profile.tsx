b108bdd5e330a12d201ac6c32e4ef42152b3990d
forest-k <bstnqls@naver.com>
2024-06-03 23:39:42 +0900

    feat: 사용자 정보 context api로 관리

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,10 +1,13 @@
+import AuthContext from "context/AuthContext";
 import { getAuth, signOut } from "firebase/auth";
 import { app } from "firebaseApp";
+import { useContext } from "react";
 import { toast } from "react-toastify";
 
 export default function Profile() {
-  const auth = getAuth(app);
+  const { user } = useContext(AuthContext)
 
+  console.log(user);
   const onSignOut = async () => {
     try {
       const auth = getAuth(app);
@@ -20,8 +23,8 @@ export default function Profile() {
       <div className="flex__box-lg">
         <div className="profile__image"></div>
         <div>
-          <div className="profile__email">{auth?.currentUser?.email}</div>
-          <div className="profile__name">{auth?.currentUser?.displayName || '사용자'}</div>
+          <div className="profile__email">{user?.email}</div>
+          <div className="profile__name">{user?.displayName || '사용자'}</div>
         </div>
       </div>
       <div role="presentation" className="profile__logout" onClick={onSignOut} >
